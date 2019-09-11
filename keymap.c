6ed5c4d0d868df7f408413348e63d937c70c0731
Gary Mialaret <gary.mialaret@gmail.com>
2019-09-12 03:33:30 +0900

    Adding capslock on the left Splyce key

--- keymap.c
+++ keymap.c
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[MAC_BASE] = LAYOUT_4key_2u_inner(
 		KC_GRV,	KC_1,	  KC_2,	  KC_3,	  KC_4, 	KC_5,	  KC_PGUP,		KC_PGDN,KC_6,   KC_7,	  KC_8,   KC_9,	  KC_0,	  KC_LBRC,
 		KC_MINS,KC_Q,	  KC_W,	  KC_E,	  KC_R,	  KC_T,	  KC_PGDN,		KC_DOWN,KC_Y,   KC_U,	  KC_I,	  KC_O,	  KC_P,	  KC_RBRC,
-		KC_TAB, KC_A,	  KC_S,	  KC_D,	  KC_F,	  KC_G,	  XXXXXXX,		KC_PSCR,KC_H,   KC_J,	  KC_K,	  KC_L,	  KC_SCLN,KC_QUOT,
+		KC_TAB, KC_A,	  KC_S,	  KC_D,	  KC_F,	  KC_G,	  KC_CAPS,		KC_PSCR,KC_H,   KC_J,	  KC_K,	  KC_L,	  KC_SCLN,KC_QUOT,
 		KC_EQL,	KC_Z,	  KC_X,	  KC_C,	  KC_V,	  KC_B,   KC_ESC,			KC_DEL,	KC_N,	  KC_M,	  KC_COMM,KC_DOT, KC_SLSH,KC_BSLS,
 		M_BASE, W_BASE, TO_GAME,KC_LCTL,M_NAV,  SP_ENT,	ALT_UDS,    GUI_GIS,KC_BSPC,KC_RSFT,CTL_CFT,KC_LEFT,KC_MINS,KC_EQL
 		),
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [WIN_BASE] = LAYOUT_4key_2u_inner(
     _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
-    _______,_______,_______,_______,_______,_______,XXXXXXX,    _______,_______,_______,_______,_______,_______,_______,
+    _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,W_NAV,  _______,ALT_UDS,    CTL_CFT,_______,_______,GUI_GIS,_______,_______,_______
     ),
